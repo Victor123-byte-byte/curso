5c29fff387075650d3972d44d10cf549219bbfdc
Victor123-byte-byte <pimentelvictorenrrique@gmail.com>
2025-07-24 20:52:26 -0400

    Add files via upload

--- tarea.py
+++ tarea.py
@@ -1,7 +1,11 @@
-edad = 20
-if edad < 20:
-  print("¿Eres menor de edad?")
-elif edad == 20:
-     print("¡Justo tienes 18 años!")
-else:
-    print("Eres mayor de edad.")
+print("\n---Ejerciocio 1: Contador de letras y consonantes---")
+frase = input("Ingresa una frase: ")
+vocales = 0
+consonantes = 0
+vocales_lista = "aeiouáéíóú"
+for letra in frase.lower():
+    if letra in vocales_lista:
+        vocales += 1
+    elif letra.isalpha():
+        consonantes += 1
+print(f"La frase '{frase}' tiene {vocales} vocales y {consonantes} consonantes.")
