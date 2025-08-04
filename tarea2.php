7d06b7c52c7bff70ea7678349dee3f3ba9eaf477
Victor123-byte-byte <pimentelvictorenrrique@gmail.com>
2025-08-04 10:48:51 -0400

    Add files via upload

---
+++ tarea2.php
@@ -0,0 +1,11 @@
+<?php
+
+$sumaTotal = 0;
+
+for ($i = 1; $i <= 100; $i++) {
+    $sumaTotal += $i; // Sumar el valor actual de $i a la suma total
+}
+
+echo "La suma de todos los números del 1 al 100 es: " . $sumaTotal;
+
+?>
